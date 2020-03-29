e07286babd519d6c5eb60e6ce64e8842ef7c110d
mattgoldsmith <goldsmithmj91@gmail.com>
2020-03-29 14:39:04 +0700

    Create new branch for tts, add tts

--- Python/CastleGame/CastleGame.py
+++ Python/CastleGame/CastleGame.py
@@ -1,3 +1,14 @@
+# Cross platform package
+import pyttsx3
+engine = pyttsx3.init()
+#engine.say('Good morning.')
+#engine.runAndWait()
+
+# Windows package
+#import win32com.client as wincl
+#speak = wincl.Dispatch("SAPI.SpVoice")
+#speak.Speak("Hello World")
+
 x = ''
 Compass = ["north", "east", "south", "west"]
 #Rooms = ["Entrance Hall", "Corridor", "Kitchen", "Armory", "Ballroom", "Smithy", "Lookout", "Dungeon"]
@@ -123,11 +134,40 @@ def useItem(item, bag):
 def win():
     print("Congratulations! You escaped from the spooky castle. Will you brave the castle once more and play again?")
 
+#Ask if they want TTS
+print("Text to speech is enabled. Would you like to use text to speech? [Yes]/[No]")
+engine.say("Text to speech is enabled. Would you like to use text to speech? [Yes]/[No]")
+engine.runAndWait()
+x = str(input()).strip()
+if x.lower() == "yes":
+    tts = True
+else:
+    tts = False
+
 print("You enter a spooky castle")
+if tts == True:
+    engine.say("You enter a spooky castle")
+    engine.runAndWait()
 print("You can move either North, South, East or West")
+if tts == True:
+    engine.say("You can move either North, South, East or West")
+    engine.runAndWait()
 print("You must find a key to exit the castle")
+if tts == True:
+    engine.say("You must find a key to exit the castle")
+    engine.runAndWait()
+print("To pick up an item, use the keyword 'take' followed by 'item_name'")
+if tts == True:
+    engine.say("To pick up an item, use the keyword 'take' followed by 'item_name'")
+    engine.runAndWait()
 print("Enter 'quit' to exit")
+if tts == True:
+    engine.say("Enter 'quit' to exit")
+    engine.runAndWait()
 print("Please enter a direction to move")
+if tts == True:
+    engine.say("Please enter a direction to move")
+    engine.runAndWait()
 print("-----------")
 roomDesc(Current_room)
 moved = 1
@@ -149,35 +189,41 @@ while x.lower() != "quit":
             print(itemDesc(Current_room))
         #Take the item
         if x.lower().split()[0].strip() == "take":
-            if Items.get(Current_room) != None:
-                if Items.get(Current_room).lower() == x.lower().split()[1]:
-                    print("You picked up the " + Items.get(Current_room))
-                    Bag.append(Items.get(Current_room).lower())
-                    Items.pop(Current_room)
-                    print(Bag)
+            if len(x.lower().split()) > 1: 
+                if Items.get(Current_room) != None:
+                    if Items.get(Current_room).lower() == x.lower().split()[1]:
+                        print("You picked up the " + Items.get(Current_room))
+                        Bag.append(Items.get(Current_room).lower())
+                        Items.pop(Current_room)
+                        print(Bag)
+                    else:
+                        print("That item doesn't exist!")
                 else:
-                    print("That item doesn't exist!")
-            else:
-                print("No useable item in this room")
+                    print("No useable item in this room")
+            else: 
+                print("Please enter an item to take")
         #Use item
         if x.lower().split()[0] == "use":
-            #No item message
-            if x.lower().split()[1].strip() not in Bag:
-                print("You don't have this item")
-            #Check if you are using the item in the correct room
-            inRoom = False
-            for key in Use_items:
-                if Use_items.get(key).lower() == x.lower().split()[1].strip() and key == Current_room:
-                    inRoom = True
-            if inRoom == False and x.lower().split()[1].strip() in Bag:
-                print("This isn't the time to use that!")
-            #Use the item
-            if inRoom == True and x.lower().split()[1].strip() in Bag:
-                xCopy = x
-                Bag, x = useItem(x.lower().split()[1].strip(), Bag)
-                #Check if the returned value is not the exit clause
-                if x == '':
-                    x = xCopy
+            if len(x.lower().split()) > 1:
+                #No item message
+                if x.lower().split()[1].strip() not in Bag:
+                    print("You don't have this item")
+                #Check if you are using the item in the correct room
+                inRoom = False
+                for key in Use_items:
+                    if Use_items.get(key).lower() == x.lower().split()[1].strip() and key == Current_room:
+                        inRoom = True
+                if inRoom == False and x.lower().split()[1].strip() in Bag:
+                    print("This isn't the time to use that!")
+                #Use the item
+                if inRoom == True and x.lower().split()[1].strip() in Bag:
+                    xCopy = x
+                    Bag, x = useItem(x.lower().split()[1].strip(), Bag)
+                    #Check if the returned value is not the exit clause
+                    if x == '':
+                        x = xCopy
+            else:
+                print("Please enter an item to use")
         #Move to a new room
         if x.lower() in directions_low:
             print("You went " + str(x))
